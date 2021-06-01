cc56861f9b6a0dc33db7ceb4fe07c033a70f5307
chiwon <ccu12345@naver.com>
2021-06-01 12:51:07 +0900

    4교시 커밋

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -1,22 +1,19 @@
 package com.edu.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * 이 클래스는 MVC웹프로젝트를 최초로 생성시 자동으로 생성되는 클래스
+ * 웹브라우저의 요처사항을 view단(jsp)에 연결시켜주는 클레스@Controller.
+ * 스프링에서 관리하는 클래스를 스프링빈(콩) = 스프링빈을 명시 @Controller. 애노테이션
+ * Beans(콩들) 그래프로 이 프로젝트의 규모를 확인가능.
+ * 스프링이 관리하는 클레스는 파일아이콘에 S가 붙습니다.
  */
+
 @Controller
 public class HomeController {
-	
+	//스프링빈(클래스) 에서는 로거로 디버그를 합니다.=로거객체를 만듭니다.
+//	private Logger logger = Logger
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
