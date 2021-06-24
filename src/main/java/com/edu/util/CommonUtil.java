bcf81761361354c4012c376b06dd75773539b12d
chiochiwo <ccu12345@naver.com>
2021-06-24 09:52:35 +0900

    1교시 커밋

--- src/main/java/com/edu/util/CommonUtil.java
+++ src/main/java/com/edu/util/CommonUtil.java
@@ -1,6 +1,7 @@
 package com.edu.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -22,14 +23,14 @@ import com.edu.service.IF_MemberService;
 import com.edu.vo.MemberVO;
 
 /**
- * 이 클래스는 이 프로젝트에서 공통으로 사용하는 유틸리티기능을 모아놓은 클래스.
+ * 이 클래스는 이 프로젝트에서 공통으로 사용하는 유틸리티기능을 모아놓은 클래스. 
  * @author 최치원
- * 컨트롤러 기능 @Controller(jsp와 바인딩이 필요할때 )
- * 콤포넌트 @Component는 MVC가 아닌 기능들을 모아놓은 스프링빈 명시, 여기서는 jsp와 바인딩이 필요해서 사용않함
+ * 컨트롤러 기능 @Controller(jsp와 바인딩이 필요할때는 필수 애노테이션 입니다.)
+ * 콤포턴트 @Component는 MVC가 아닌 기능들을 모아놓은 스프링빈 명시, 여기서는 jsp와 바인딩이 필요해서 사용않함 
  */
 @Controller
 public class CommonUtil {
-	//맴버변수생성(아래)
+	//멤버변수생성(아래)
 	private Logger logger = LoggerFactory.getLogger(CommonUtil.class);
 	@Inject
 	private IF_MemberService memberService;//스프링빈을 주입받아서(DI) 객체준비
@@ -53,14 +54,13 @@ public class CommonUtil {
         ret = ret.replaceAll("</(F|f)(O|o)(R|r)(M|m)", "&lt;form");
 		return ret;
 	}
-	//첨부파일 업로드/다운로드/삭제/인서트/수정에 모두 사용될 저장경로를 1개지정해서 전역으로 사용
+	//첨부파일 업로드/다운로드/삭제/인서트/수정에 모두 사용될 저장경로를 1개지정해서 [전역]으로사용
 	@Resource(name="uploadPath")
 	private String uploadPath;//root-context 업로드경로 클래스빈의 id값을 받아서 String변수 입력
-	
 	public String getUploadPath() {
 		return uploadPath;
 	}
-	
+
 	//첨부파일이 이미지인지 아닌지 확인하는 데이터생성
 	private ArrayList<String> checkImgArray = new ArrayList<String>() {
 		{
@@ -74,31 +74,36 @@ public class CommonUtil {
 	public ArrayList<String> getCheckImgArray() {
 		return checkImgArray;
 	}
-	//RestAPI서버 맛보기 (제대로 만들면 @RestCOntroller 사용)
+	
+	//RestAPI서버 맛보기ID중복체크(제대로 만들면 @RestController 사용)
 	@RequestMapping(value="/id_check", method=RequestMethod.GET)
 	@ResponseBody //반환받은 값의 헤더값을 제외하고, 내용(body)만 반환하겠다는 명시
 	public String id_check(@RequestParam("user_id")String user_id) throws Exception {
-		//중복아이디를 체크로직(아래)
-		String memberCnt = "1";//중복ID가 없을떄, 기본값1
-		if(!user_id.isEmpty()) {//!주의user_id가 공백이 아니라면,
+		//중복아이디를 체크로지(아래)
+		String memberCnt = "1";//중복ID가 있을때, 기본값 1
+		if(!user_id.isEmpty()) {//!주의 user_id가 공백이 아니라면,
 			MemberVO memberVO = memberService.readMember(user_id);
-			logger.info("디버그: " + memberVO);//user_id를 공백을 전송해도null이기때문에 조건이 1개필ㄹ요
-			if(memberVO == null) { //중복아이디가 존재하지 않으면 {}안을 실행
+			logger.info("디버그: " + memberVO);//user_id를 공백을 전송해도 null이기때문에 조건 추가필요
+			if(memberVO == null) {//중복아이디가 존재하지 않으면 {}안을 실행
 				memberCnt = "0";
 			}
 		}
 		return memberCnt;//0.jsp 이렇게 작동하지 않습니다. 이유는 @ResponseBody때문이고, RestAPI는 값만 반환
 	}
-	//파일 업로드 공통 메서드(Admin컨트롤러에서사용 + Home컨트롤러에서도 사용)
-	public String fileUpload(MultipartFile file) {
-		// TODO UUID클래스로 저장됨 고유식별(PK) 파일명을 생성 후 물리적으로 저장
+
+	//파일 업로드 공통 메서드(Admin컨트롤러에서 사용 + Home컨트롤러에서도 사용)
+	public String fileUpload(MultipartFile file) throws IOException {
+		// TODO UUID클래스로 저장될 고유식별(PK) 파일명을 생성 후 물리적으로 저장
 		String realFileName = file.getOriginalFilename();
 		// 폴더에 저장할 PK파일명을 생성(아래)
 		UUID uid = UUID.randomUUID();//유니크ID값 생성
 		String saveFileName = uid.toString() + "." + StringUtils.getFilenameExtension(realFileName);
-		byte[] fileData = file.getBytes();
+		//file의 MultipartFile클래스형 객체. 클래스형 자료(멤버변수,메서드..)는 직접 파일저장을 할 수 없음.
+		//그래서, 바이트형으로 파싱(변환)해서 저장해야함. -> bit형(010100001)이진 비트형 자료로 변환필요
+		//자바자료형 정수: byte(bit로구성)<short<int<long, 실수형(소수점): float<double
+		byte[] fileData = file.getBytes();//getBytes메서드를 데이터를 bit형으로 파싱해서 저장
 		File target = new File(uploadPath, saveFileName);
 		FileCopyUtils.copy(fileData, target);//파일이 물리적으로 폴더에 저장됨.
-		return saveFileName;
+		return saveFileName;//UUID로 생성된 식별값의 파일명 
 	}
 }
